c5c322acd625258f497e64f825bf1e9c5d015889
Kyle <kylei.tw@gmail.ocm>
2021-07-29 11:48:24 +0800

    Optmization

--- The Dark Knight/AppDelegate.swift
+++ The Dark Knight/AppDelegate.swift
@@ -14,6 +14,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UITabBar.appearance().unselectedItemTintColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.87) //設定其他未選擇的tab顏色
+        UITabBar.appearance().backgroundColor = .clear //將背景顏色設位透明
+        UITabBar.appearance().backgroundImage = UIImage() //使用空圖片將背景設定為透明
+        UITabBar.appearance().shadowImage = UIImage() //使用空圖片將陰影設定為透明
+        UITabBar.appearance().tintColor = .orange //設置當前頁面的tab顏色
+        
+        
+        UINavigationBar.appearance().isTranslucent = true //預設已是半透明，再次聲明可避免其它更動
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default) //使用空圖片將背景設定為透明
+        UINavigationBar.appearance().shadowImage = UIImage() //使用空圖片將陰影設定為透明
+        
+        
+        
+        UINavigationBar.appearance().tintColor = .orange //設置backindicator顏色
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.orange]  //設置title的顏色
+        
+        
         return true
     }
 
--- The Dark Knight/ViewController.swift
+++ The Dark Knight/ViewController.swift
@@ -8,128 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+                      
+        
     }
 }
 
-
-class NavigationController: UINavigationController {
-    @IBOutlet var navigation: UINavigationBar!{
-        didSet{
-            navigation.isTranslucent = true //預設已是半透明，再次聲明可避免其它更動
-            navigation.setBackgroundImage(UIImage(), for: .default) //使用空圖片將背景設定為透明
-            navigation.shadowImage = UIImage() //使用空圖片將陰影設定為透明
-            
-            
-            
-            navigation.tintColor = .orange //設置backindicator顏色
-            navigation.titleTextAttributes = [.foregroundColor: UIColor.orange]  //設置title的顏色
-        }
-    }
-}
-
-class TabBarController: UITabBarController {
-    @IBOutlet var tab: UITabBar!{
-        didSet{
-            tab.backgroundColor = .clear //將背景顏色設位透明
-            tab.backgroundImage = UIImage() //使用空圖片將背景設定為透明
-            tab.shadowImage = UIImage() //使用空圖片將陰影設定為透明
-      
-            tab.tintColor = .orange //設置當前頁面的tab顏色
-            tab.unselectedItemTintColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.87) //設定其他未選擇的tab顏色
-  
-        }
-    }
-}
-
-
-
-class CastUIView: UITableView {
-    @IBOutlet var ImageView: UIImageView! {
-        didSet {
-            ImageView.layer.cornerRadius = 24 //設定imageView的圓角
-            ImageView.clipsToBounds = true //設定圖片能被定義為圓角的邊界裁切點掉
-        }
-    }
-    
-    @IBOutlet var ImageView2: UIImageView! {
-        didSet {
-            ImageView2.layer.cornerRadius = 24
-            ImageView2.clipsToBounds = true
-        }
-    }
-    
-    
-    @IBOutlet var ImageView3: UIImageView! {
-        didSet {
-            ImageView3.layer.cornerRadius = 24
-            ImageView3.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView4: UIImageView! {
-        didSet {
-            ImageView4.layer.cornerRadius = 24
-            ImageView4.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView5: UIImageView! {
-        didSet {
-            ImageView5.layer.cornerRadius = 24
-            ImageView5.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView6: UIImageView! {
-        didSet {
-            ImageView6.layer.cornerRadius = 24
-            ImageView6.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView7: UIImageView! {
-        didSet {
-            ImageView7.layer.cornerRadius = 24
-            ImageView7.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView8: UIImageView! {
-        didSet {
-            ImageView8.layer.cornerRadius = 24
-            ImageView8.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView9: UIImageView! {
-        didSet {
-            ImageView9.layer.cornerRadius = 24
-            ImageView9.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView10: UIImageView! {
-        didSet {
-            ImageView10.layer.cornerRadius = 24
-            ImageView10.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView11: UIImageView! {
-        didSet {
-            ImageView11.layer.cornerRadius = 24
-            ImageView11.clipsToBounds = true
-        }
-    }
-    
-    @IBOutlet var ImageView12: UIImageView! {
-        didSet {
-            ImageView12.layer.cornerRadius = 24
-            ImageView12.clipsToBounds = true
-        }
-    }
-}
